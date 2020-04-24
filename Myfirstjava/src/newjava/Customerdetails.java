0c30359fb27dd21b7f09d61a6edf7be1cc9ac72f
Ajinkya <Ajinkya@DESKTOP-5JAHJQ2>
2020-04-24 11:15:37 +0530

    codes

---
+++ Myfirstjava/src/newjava/Customerdetails.java
@@ -0,0 +1,12 @@
+package newjava;
+
+public class Customerdetails {
+	int CustomerId;
+	String CustomerName;
+	Double CustomerMobileNumber;
+	
+	public void addcustomer() {
+		
+	}
+
+}
---
+++ Myfirstjava/src/newjava/HelloWorld.java
@@ -0,0 +1,8 @@
+package newjava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+	}
+
+}
